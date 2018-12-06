4fda5bf63dd1f838589ab8bd40894401203cecb7
Michael Abbott <improbable+gh@gmail.com>
2018-12-06 18:20:34 +0100

    all

--- src/RunMyNotes.jl
+++ src/RunMyNotes.jl
@@ -9,31 +9,44 @@ using Literate, Conda
 Runs all files `*.jl` found in folder `in`, generating a Jupyter `.ipynb` notebook for each,
 saved in the same folder or in `out` if provided.
 
-Keywords `html=true` then converts this notebook to HTML too,
-`throw=true` exits with an error if any errors were encountered in doing this.
-Some details about time & computer are printed to the file when 'verbose=true`, and `credit=true` adds links.
+Keywords: 
+* `html=true` then converts this notebook to HTML too, using `jupyter nbconvert`.
+* `throw=true` exits with an error if any errors were encountered .
+* `all=true` runs everything, `all=false` will skip any `.jl` files which are older than their `.ipynb` versions.
+* 'verbose=true` adds details about time & computer at the end.
+* `credit=true` adds links.
 """
-function folder(indir::String, outdir::String=normpath(indir);
+function folder(indir::String, outdir::String=indir;
         html::Bool=true, throw::Bool=true, all::Bool=true, credit::Bool=true, verbose::Bool=true)
 
-    fulldir = normpath(indir)
-    @assert isdir(fulldir) && isdir(outdir) "you need to give a directory name"
+    fulldir = normpath(expanduser(indir))
+    fullout = normpath(expanduser(outdir))
+    @assert isdir(fulldir) "can't find input folder $fulldir"
+    @assert isdir(fullout) "can't find output folder $fullout"
+    
+    olddir = pwd() # should just fix run command instead
 
     pre = verbose ? appendinfo : identity
     post = credit ? appendcredit : identity
 
-    all || @error "all=false is ignored for now"
     list = filter(s->endswith(s,".jl"), readdir(fulldir))
 
     errs = []
 
     for name in list
+        jlname = joinpath(fulldir, name)
+        nbname = joinpath(fullout, name)[1:end-2] * "ipynb"
+        
+        if isfile(nbname) && mtime(nbname) > mtime(jlname)
+            all || continue # all=false means skip files for which .jl hasn't changed
+        end
+        
         ran = true
         nb = ""
         try
-            nb = Literate.notebook(joinpath(fulldir, name), outdir; credit=credit, preprocess=pre, postprocess=post)
+            nb = Literate.notebook(jlname, fullout; credit=credit, preprocess=pre, postprocess=post)
         catch err
-            @error "error in running notebook $(name): \n" err
+            @error "error in creating or running notebook from $jlname \n" err
             push!(errs, err)
             ran = false
         end
@@ -41,13 +54,15 @@ function folder(indir::String, outdir::String=normpath(indir);
         if html && ran
             try
                 cd(Conda.PYTHONDIR)
-                run(`./jupyter nbconvert $nb`)
+                run(`./jupyter nbconvert $nb`) # writes to same folder
             catch err
-                @error "error in converting notebook $(name): \n" err
+                @error "error in converting notebook $nb \n" err
                 push!(errs, err)
             end
         end
     end
+    
+    cd(olddir)
 
     if throw && length(errs)>0
         error("encountered $(length(errs)) errors executing & converting notebooks, details printed above")
@@ -64,12 +79,12 @@ Change keyword `sub="notes"` to select a different folder.
 """
 function package(mod::Module, outdir=nothing; sub::String="notes", kw...)
 
-    indir = joinpath(dirname(pathof(mod)), "..", sub)
+    indir = joinpath(dirname(pathof(mod)), "..", sub) # pathof gives "...dev/src/RunMyNotes.jl"
     if outdir==nothing
-        outdir = normpath(indir)
+        outdir = indir # these will get expanduser |> normpath
     end
 
-    return folder(indir, outdir, kw...)
+    return folder(indir, outdir; kw...)
 end
 
 using Dates, InteractiveUtils
